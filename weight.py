3b7829c07786ba8b6d21fc80963753c58cd1b82f
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-01-01 17:59:43 +0100

    wersja do sprawdzenia

--- weight.py
+++ weight.py
@@ -1,9 +1,8 @@
 container_weight = 0
 container_count = 0
 total_weight = 0
-current_emptiness = 0
 biggest_emptiness = 0
-pack_number = 0
+pack_number = 1 #Jedynka dla przypadku tylko jednej nie do końca załadowanej paczki
 
 print("Podaj liczbę paczek do wysyłki.")
 number_of_packs = int(input())
@@ -12,7 +11,6 @@ for load in range(number_of_packs):
 
     print("Podaj wagę elementu (min 1, max 10).")
     pack_weight = int(input())
-    print("....................................")
     total_weight += pack_weight
 
     if pack_weight == 0:
@@ -22,8 +20,6 @@ for load in range(number_of_packs):
         break
     elif 1 <= pack_weight <= 10:
         container_weight += pack_weight
-        current_emptiness = 20 - container_weight
-        print("najwieksza pustka: {}".format(biggest_emptiness))
     if container_weight > 20:
         container_count += 1
         if biggest_emptiness < 20 - (container_weight - pack_weight):
@@ -31,27 +27,14 @@ for load in range(number_of_packs):
             pack_number = container_count
         container_weight = 0
         container_weight += pack_weight
-        print("petla > 20 {}".format(pack_weight))
-        current_emptiness = 20 - container_weight
-        print("petla >20: {}".format(biggest_emptiness))
     if container_weight < 20 and load == number_of_packs - 1:
+        container_count += 1
         if biggest_emptiness < 20 - container_weight:
             biggest_emptiness = 20 - container_weight
             pack_number = container_count
-        container_count += 1
-        print("petla ostatnia: {}".format(biggest_emptiness))
     if container_weight == 20:
         container_count +=1
         container_weight = 0
-        current_emptiness = 0
-        print("petla == 20: {}".format(biggest_emptiness))
-
-    print("bieżąca ilość wysłanych paczek {}".format(container_count))
-    print("bieżąca waga kontenera {}".format(container_weight))
-    print("bieżąca liczba kg: {}".format(total_weight))
-    print("bieżąca liczba pustych kg: {}".format(current_emptiness))
-    print("numer najbardziej pustej paczki: {}".format(pack_number))
-    print("....................................")
 
 pack_sent = f'Ilość wysłanych paczek: {container_count}'
 kg_sent = f'Ilość wysłanych kilogramów: {total_weight}'
@@ -59,7 +42,8 @@ wasted_kg = f'Liczba pustych kilogramów: {container_count * 20 - total_weight}'
 worst_pack_number = f'Numer najgorzej spakowanej paczki: {pack_number}'
 worst_pack = f'Najwięcej pustych kilogramów na paczkę: {biggest_emptiness}'
 
-if biggest_emptiness == 0:
+
+if biggest_emptiness == 0: #Przypadek gdy paczki są idealnie zapakowane
     worst_pack_number = 'Numer najgorzej spakowanej paczki: brak'
     worst_pack = 'Najwięcej pustych kilogramów na paczkę: brak'
     wasted_kg = f'Liczba pustych kilogramów: brak'
