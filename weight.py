2d08a1be114853018130d526a64c36392f3b4b44
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-01-01 18:43:47 +0100

    poprawiłem obsługę pustych paczek przy niepoprawnych wartościach wagi

--- weight.py
+++ weight.py
@@ -2,7 +2,7 @@ container_weight = 0
 container_count = 0
 total_weight = 0
 biggest_emptiness = 0
-pack_number = 1 #Jedynka dla przypadku tylko jednej nie do końca załadowanej paczki
+pack_number = 1  # Jedynka dla przypadku tylko jednej nie do końca załadowanej paczki
 
 print("Podaj liczbę paczek do wysyłki.")
 number_of_packs = int(input())
@@ -11,14 +11,28 @@ for load in range(number_of_packs):
 
     print("Podaj wagę elementu (min 1, max 10).")
     pack_weight = int(input())
-    total_weight += pack_weight
+
+    # Obsługa błędów
 
     if pack_weight == 0:
+        if container_weight > 0:
+            container_count += 1
+        if biggest_emptiness < 20 - container_weight:
+            biggest_emptiness = 20 - container_weight
+            pack_number = container_count
         break
     elif pack_weight < 1 or pack_weight > 10:
+        if container_weight > 0:
+            container_count += 1
+        if biggest_emptiness < 20 - container_weight:
+            biggest_emptiness = 20 - container_weight
+            pack_number = container_count
         print("Błąd! Zła waga!")
         break
-    elif 1 <= pack_weight <= 10:
+
+    total_weight += pack_weight  # Dodane dopiero teraz, żeby nie dodawać błędnej wartości wagi do całości
+
+    if 1 <= pack_weight <= 10:
         container_weight += pack_weight
     if container_weight > 20:
         container_count += 1
@@ -33,7 +47,7 @@ for load in range(number_of_packs):
             biggest_emptiness = 20 - container_weight
             pack_number = container_count
     if container_weight == 20:
-        container_count +=1
+        container_count += 1
         container_weight = 0
 
 pack_sent = f'Ilość wysłanych paczek: {container_count}'
@@ -43,11 +57,11 @@ worst_pack_number = f'Numer najgorzej spakowanej paczki: {pack_number}'
 worst_pack = f'Najwięcej pustych kilogramów na paczkę: {biggest_emptiness}'
 
 
-if biggest_emptiness == 0: #Przypadek gdy paczki są idealnie zapakowane
+if biggest_emptiness == 0:  # Przypadek gdy paczki są idealnie zapakowane
     worst_pack_number = 'Numer najgorzej spakowanej paczki: brak'
     worst_pack = 'Najwięcej pustych kilogramów na paczkę: brak'
     wasted_kg = f'Liczba pustych kilogramów: brak'
-if number_of_packs >=0:
+if number_of_packs >= 0:
     print(pack_sent)
     print(kg_sent)
     print(wasted_kg)
