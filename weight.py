2af4f65a0726c03c8da8fc01ffcaf0f49ced15a4
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-01-02 07:40:33 +0100

    kolejne poprawki obsługi błędów

--- weight.py
+++ weight.py
@@ -8,30 +8,26 @@ print("Podaj liczbę paczek do wysyłki.")
 number_of_packs = int(input())
 
 for load in range(number_of_packs):
-
     print("Podaj wagę elementu (min 1, max 10).")
     pack_weight = int(input())
 
-    # Obsługa błędów
+# Obsługa błędów
 
-    if pack_weight == 0:
+    if pack_weight == 0 or pack_weight < 1 or pack_weight > 10:
         if container_weight > 0:
             container_count += 1
-        if biggest_emptiness < 20 - container_weight:
+        if biggest_emptiness < 20 - container_weight and load > 0:
             biggest_emptiness = 20 - container_weight
             pack_number = container_count
-        break
-    elif pack_weight < 1 or pack_weight > 10:
-        if container_weight > 0:
-            container_count += 1
-        if biggest_emptiness < 20 - container_weight:
-            biggest_emptiness = 20 - container_weight
-            pack_number = container_count
-        print("Błąd! Zła waga!")
-        break
+        if pack_weight == 0:
+            break
+        else:
+            print("Błąd! Zła waga!")
+            break
+
+# Właściwy program
 
     total_weight += pack_weight  # Dodane dopiero teraz, żeby nie dodawać błędnej wartości wagi do całości
-
     if 1 <= pack_weight <= 10:
         container_weight += pack_weight
     if container_weight > 20:
@@ -50,12 +46,13 @@ for load in range(number_of_packs):
         container_count += 1
         container_weight = 0
 
+# Drukowanie
+
 pack_sent = f'Ilość wysłanych paczek: {container_count}'
 kg_sent = f'Ilość wysłanych kilogramów: {total_weight}'
 wasted_kg = f'Liczba pustych kilogramów: {container_count * 20 - total_weight}'
 worst_pack_number = f'Numer najgorzej spakowanej paczki: {pack_number}'
 worst_pack = f'Najwięcej pustych kilogramów na paczkę: {biggest_emptiness}'
-
 
 if biggest_emptiness == 0:  # Przypadek gdy paczki są idealnie zapakowane
     worst_pack_number = 'Numer najgorzej spakowanej paczki: brak'
